fea4de6ee110ccf28ad308e36c7df11f4b3d165c
Colin Edwards <ColinJhEdwards@gmail.com>
2022-05-04 19:33:14 -0700

    set up inital fetch function using type script for specifying types

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FormEvent, useEffect, useState } from "react";
+import { IRecipe } from "./IRecipe";
 
 function App() {
+  const apiKey = process.env.REACT_APP_API_KEY;
+  console.log(apiKey);
+  const [recipesFound, setRecipesFound] = useState<IRecipe[]>([]);
+  const [recipeSearch, setRecipeSearch] = useState("");
+
+  const searchForRecipes = async (search: string): Promise<IRecipe[]> => {
+    const result = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apikey=${apiKey}&query=${search}`
+    );
+    return (await result.json()).results;
+  };
+
+  const search = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = e.target as HTMLFormElement;
+    const input = form.querySelector("#searchText") as HTMLInputElement;
+    setRecipeSearch(input.value);
+  };
+
+  useEffect(() => {
+    (async () => {
+      const search = encodeURIComponent(recipeSearch);
+      if (search) {
+        const response = await searchForRecipes(search);
+        setRecipesFound(response);
+      }
+    })();
+  }, [recipeSearch]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Hello world</h2>
+      <form className="searchForm" onSubmit={(e) => search(e)}>
+        <input type="text" id="searchText" />
+        <button>Search</button>
+      </form>
     </div>
   );
 }
---
+++ src/IRecipe.ts
@@ -0,0 +1,6 @@
+export interface IRecipe {
+  href: string;
+  ingrdients: string;
+  thumbnail: string;
+  title: string;
+}
